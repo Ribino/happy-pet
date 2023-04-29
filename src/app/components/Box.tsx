f9906b15391416020d122617a4332b560fd739b3
Lucas Ribino <lribino@outlook.com>
2023-04-29 18:12:14 -0300

    create calendar component and all customization

--- src/app/components/Box.tsx
+++ src/app/components/Box.tsx
@@ -2,9 +2,10 @@ import { isEmpty } from "lodash";
 import { ReactNode } from "react";
 
 interface Props {
-headerTitle?: string;
-emptyMessage?: string;
-children?: ReactNode;
+   headerTitle?: string
+   emptyMessage?: string
+   children?: ReactNode
+   className?: string
 }
 
 export default function Box(props: Props) {
@@ -12,14 +13,14 @@ export default function Box(props: Props) {
    const emptyInfo = isEmpty(props.children);
    return (
    <>
-      <div className="flex flex-col h-5/6">
+      <div className={`flex flex-col drop-shadow-lg border border-zinc-300 rounded-3xl overflow-hidden h-full ${props?.className ?? ''}`}>
          {shouldDisplayHeader && (
-            <div className="h-24 bg-zinc-200 rounded-t-3xl flex justify-center items-center">
+            <div className="h-24 bg-zinc-200 flex justify-center items-center">
                <span className="font-bold text-xl">{props.headerTitle}</span>
             </div>
          )}
 
-         <div className="bg-zinc-100 rounded-b-3xl overflow-auto h-full">
+         <div className="bg-zinc-100 overflow-auto h-full">
          {emptyInfo  
             ?  <div className="h-full flex justify-center items-center italic text-neutral-600 text-sm">
                   {props.emptyMessage}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Row from "./components/Row";
 import Column from "./components/Column";
 import Field from "./components/Field";
 import Button from "./components/Button";
+import Calendar from "./components/Calendar/Calendar";
 
 export default function Home() {
   const dataEmpty: any[] = []
@@ -20,12 +21,27 @@ export default function Home() {
               ]
   const existData = data.length > 0 
 
+
+  const datesWithInfo = [
+      new Date(2023, 4, 10),
+      new Date(2023, 4, 16),
+      new Date(2023, 5, 23) 
+  ]
+
    return (
       <main className="h-screen w-screen">
-         <div className="grid xl:grid-cols-2 lg:grid-cols-1 lg:mx-40 xl:mx-20 gap-y-20 gap-x-20 mt-10 h-3/5 ">
-         <Box headerTitle="Agendamentos" emptyMessage="Calendario">
+         <div className="grid xl:grid-cols-5 lg:grid-cols-1 lg:mx-40 xl:mx-20 gap-y-20 gap-x-20 mt-10 h-3/6">
+         <Box headerTitle="Agendamentos" emptyMessage="Calendario" className="xl:col-span-2">
+            <div className="flex flex-col justify-center items-center 2xl:mx-24 xl:mx-14 lg:mx-36">
+               <select placeholder="Filtrar" className="bg-orange-400 text-white h-10 w-full mt-5 rounded-lg border border-zinc-500" >
+                  <option>Filtrar</option>
+                  <option value="A">A</option>  
+                  <option value="B">B</option>
+               </select>
+               <Calendar multipleSelect hightlightDates={datesWithInfo} disabledDates={[new Date(2023, 4, 15)]}/>
+            </div>
          </Box>
-         <Box headerTitle="Detalhes" emptyMessage="Selecione uma data para visualizar os detalhes">
+         <Box headerTitle="Detalhes" emptyMessage="Selecione uma data para visualizar os detalhes" className="xl:col-span-3 xl:col-start-3">
             {
                existData &&
                data.map( (array, index) =>
