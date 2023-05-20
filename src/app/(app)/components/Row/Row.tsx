15a2833757830139c8d9b536706dec01b1305d1e
Lucas Ribino <lribino@outlook.com>
2023-05-20 16:38:02 -0300

    remove number of columns

--- src/app/(app)/components/Row/Row.tsx
+++ src/app/(app)/components/Row/Row.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 
 interface Props {
-	 numberOfColumns: number,
 	 applyShadow?: boolean,
 	 children?: ReactNode;
 }
@@ -9,7 +8,7 @@ interface Props {
 export default function Row(props: Props) {
 	 return (
 		  <>
-				<div className={`grid grid-cols-${props.numberOfColumns} m-2 rounded-xl overflow-hidden font-normal text-zinc-800 row
+				<div className={`grid grid-cols-1 m-2 rounded-xl overflow-hidden font-normal text-zinc-800 row
 									${props.applyShadow ? 'shadow-lg border-2' : ''}`}>    
 					<div className="flex gap-x-0.5 h-auto w-full" > 
 						{ props.children } 
--- src/app/(app)/components/Row/SelectRow.tsx
+++ src/app/(app)/components/Row/SelectRow.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 import _ from 'lodash'
 
 interface Props {
-    numberOfColumns: number,
     onSelected: Function,
     selected: boolean,
     children?: ReactNode,
@@ -18,12 +17,14 @@ export default function SelectRow(props: Props) {
     }
     return (
         <>
-            <div className={`grid grid-cols-${props.numberOfColumns} m-2 rounded-xl overflow-hidden font-normal text-zinc-800 row`}>
+            <div className={`grid grid-cols-1 m-2 rounded-xl overflow-hidden font-normal text-zinc-800 row`}>
                 
                 <button type="button" 
                         onClick={() => handleClick()} 
-                        className={`group/select-row flex gap-x-0.5 h-auto w-ful ${props.selected ? "bg-zinc-400 [&>*]:bg-teal-700 [&>*]:text-white [&>.icon]:text-orange-200" : ''} 
-                        ${props.className ?? ''}`}>         
+                        className={`group/select-row flex gap-x-0.5 h-auto w-full 
+                        ${props.selected ? "bg-zinc-400 [&>*]:bg-teal-700 [&>*]:text-white [&>.icon]:text-orange-200" : ''} 
+                        ${props.className ?? ''}
+                        `}>         
                     {props.children}
                 </button> 
                                
