6ca3dacdfb761005df5ecda8ec6917ace6dcff86
Lucas Ribino <lribino@outlook.com>
2023-05-29 21:28:02 -0300

    Integrate list service with back end

---
+++ src/app/(app)/scheduling/create/components/SelectService/ListService.tsx
@@ -0,0 +1,65 @@
+import React, { useRef } from "react";
+import SelectRow from "@/app/(app)/components/Row/SelectRow";
+import Column from "@/app/(app)/components/Column";
+import Field from "@/app/(app)/components/Field";
+import { ForceUpdate, decodeToken } from "../../page";
+import { isEmpty, isUndefined } from "lodash";
+
+interface Props {
+  onSelected: (service: Service) => void,
+  isServiceSelected: (service: Service) => boolean
+}
+
+export interface Service {
+  id: number;
+  name: string;
+  price: number;
+  time: number;
+  serviceType: string;
+}
+
+export default function ListService(props: Props) {
+  const update = ForceUpdate();
+  const { onSelected, isServiceSelected } = props
+  const services = useRef<Service[]>();
+  
+  async function getServices() {
+    const decode = decodeToken();
+      if(!isUndefined(decode)) {
+         const { token } = decode;
+         const res = await fetch(`${process.env.HOST}/service`, {
+            method: "GET",
+            headers: {
+               Authorization: `Bearer ${token}`
+            }
+         })
+         services.current = await res.json();
+         update();
+      }
+  }
+
+  if(isEmpty(services.current)) {
+    getServices();
+  }
+
+  return (
+    <>
+      {
+        services.current &&
+        services.current.map((service) => (
+        <SelectRow key={service.id}
+          onSelected={() => onSelected(service)}
+          selected={isServiceSelected(service)}
+        >
+          <Column flexType="flex-none" applyHighlite className="w-20 !py-3.5">
+            <Field type="price" value={service.price} />
+          </Column>
+          <Column>
+            <Field type="text" value={service.name} />
+          </Column>
+        </SelectRow>
+      ))
+      }
+    </> 
+  );
+}
--- src/app/(app)/scheduling/create/components/SelectService/SelectService.tsx
+++ src/app/(app)/scheduling/create/components/SelectService/SelectService.tsx
@@ -1,11 +1,12 @@
-import React, { Dispatch, SetStateAction } from "react";
-import Box from "../../../components/Box";
-import SelectRow from "@/app/(app)/components/Row/SelectRow";
+import React, { Dispatch, SetStateAction, Suspense } from "react";
+import Box from "../../../../components/Box";
 import Column from "@/app/(app)/components/Column";
 import Field from "@/app/(app)/components/Field";
 import Textarea from "@/app/components/Textarea";
-import { Scheduling } from "../page";
+import { Scheduling } from "../../page";
 import Row from "@/app/(app)/components/Row/Row";
+import ListService from "./ListService";
+import Loading from "@/app/components/Loading";
 
 interface Props {
   scheduling: Scheduling | undefined
@@ -23,51 +24,6 @@ export interface Service {
 export default function SelectService(props: Props) {
   const {scheduling, setScheduling} = props;
   const emptyMessage = "Nenhum serviço cadastrado no momento";
- 
-  const services = [
-    {
-      id: 2,
-      name: "Banho",
-      price: 69.99,
-      time: 30,
-      serviceType: "PETSHOP",
-    },
-    {
-      id: 3,
-      name: "Banho & Tosa Trimming",
-      price: 69.99,
-      time: 50,
-      serviceType: "PETSHOP",
-    },
-    {
-      id: 4,
-      name: "Tosa",
-      price: 20.69,
-      time: 30,
-      serviceType: "PETSHOP",
-    },
-    {
-      id: 5,
-      name: "Banho Premium",
-      price: 84.94,
-      time: 30,
-      serviceType: "PETSHOP",
-    },
-    {
-      id: 6,
-      name: "Pet sitter",
-      price: 104.94,
-      time: 30,
-      serviceType: "PETSHOP",
-    },
-    {
-      id: 7,
-      name: "Pet Walker",
-      price: 184.94,
-      time: 30,
-      serviceType: "PETSHOP",
-    },
-  ];
 
   function onSelected(service: Service) {
     const newScheduling = { 
@@ -86,19 +42,11 @@ export default function SelectService(props: Props) {
     <div className="w-full flex justify-center gap-16">
       <div className="w-full">
         <Box emptyMessage={emptyMessage} headerTitle="Serviços">
-          {services.map((service) => (
-            <SelectRow key={service.id}
-              onSelected={() => onSelected(service)}
-              selected={isServiceSelected(service)}
-            >
-              <Column flexType="flex-none" applyHighlite className="w-20 !py-3.5">
-                <Field type="price" value={service.price} />
-              </Column>
-              <Column>
-                <Field type="text" value={service.name} />
-              </Column>
-            </SelectRow>
-          ))}
+          {
+            <Suspense fallback={<Loading/>}>
+              <ListService isServiceSelected={isServiceSelected} onSelected={onSelected}/>
+            </Suspense>
+          }
         </Box>
       </div>
       <div className="w-full">
